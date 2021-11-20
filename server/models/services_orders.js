da221888badf9b2516755da9259732df8101b78c
Jazeb <jazeb.jazi007@gmail.com>
2021-11-20 21:56:16 +0500

    lat long

--- server/models/services_orders.js
+++ server/models/services_orders.js
@@ -22,6 +22,10 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING(11),
             allowNull: true
         },
+        long: {
+            type: DataTypes.STRING(11),
+            allowNull: true
+        },
         accepted_by: {
             type: DataTypes.INTEGER(11),
             allowNull: true
